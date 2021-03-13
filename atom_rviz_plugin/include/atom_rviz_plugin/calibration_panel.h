d678a61482ef1f53c6d99149fe650f543e06669c
Miguel Pina <miguelpina@ua.pt>
2021-03-13 12:31:29 +0000

    Adding a new package to test the problem issued in #10

---
+++ atom_rviz_plugin/include/atom_rviz_plugin/calibration_panel.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef Q_MOC_RUN
+#include <ros/ros.h>
+#endif
+
+#include <rviz/panel.h>
+
+namespace Ui {
+class CalibUI;
+}
+
+namespace atom_rviz_plugin
+{
+class CalibrationPanel: public rviz::Panel
+{
+  Q_OBJECT
+ public:
+  CalibrationPanel(QWidget* parent = nullptr);
+  ~CalibrationPanel() override;
+
+  void onInitialize() override;
+  void onEnable();
+  void onDisable();
+
+private Q_SLOTS:
+  void buttonClicked();
+
+protected:
+  Ui::CalibUI* ui_;
+};
+} // end namespace calibration
