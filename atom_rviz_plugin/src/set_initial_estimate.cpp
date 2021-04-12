016d72f76f4480dedb910573cea8ea305fa0bc80
Miguel Oliveira <m.riem.oliveira@gmail.com>
2021-04-12 17:39:53 +0100

    Setting up rviz pannel interaction with set_initial_estimate (#278)

--- atom_rviz_plugin/src/set_initial_estimate.cpp
+++ atom_rviz_plugin/src/set_initial_estimate.cpp
@@ -2,6 +2,8 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include <atom_msgs/SetSensorInteractiveMarker.h>
+
 
 #include "ui_calibration_panel.h"
 
@@ -25,13 +27,35 @@ namespace atom_rviz_plugin
     void CalibrationPanel::initEstimateCheckboxChanged()
     {
       // Code to call the service (client)
-//      ros::ServiceClient client = nh.serviceClient<atom_rviz_plugin::SetSensorInteractiveMarker>("set_interactive_marker");
+      std::string service_name = "/set_initial_estimate/3dlidar/set_sensor_interactive_marker"; // TODO should be changed according to the sensor that is selected
+
+      //Service clients should not be transient, which means they should be initialized in the class construction and then used here
+      // TODO Put this is the constructor
+      ros::ServiceClient client = nh.serviceClient<atom_msgs::SetSensorInteractiveMarker>(service_name);
+
+      atom_msgs::SetSensorInteractiveMarker srv;
+      //  Check http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28c%2B%2B%29
 
       if (ui_->initialEstimateCheckBox->isChecked()) {
         ROS_INFO_STREAM("show marker");
+        srv.request.visible = 1;
       } else {
         ROS_INFO_STREAM("hide marker");
+        srv.request.visible = 0;
       }
+
+      srv.request.scale = 2; // Change to the scale value that is selected here
+
+      int ret = client.call(srv);
+      std::cout << ret << std::endl;
+//      if (client.call(srv))
+//      {
+//        ROS_INFO_STREAM(srv.response.message);
+//      }
+//      else
+//      {
+//        ROS_INFO_STREAM(srv.response.message);
+//      }
 
     } //function initEstimateCheckbox()
 
