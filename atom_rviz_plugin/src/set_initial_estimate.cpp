7ca1d8d61ac616e55508d588a59b801683b6de39
Miguel Pina <miguelpina@ua.pt>
2021-04-13 16:17:53 +0100

    Code (still in progress) for set_initial_estimate

--- atom_rviz_plugin/src/set_initial_estimate.cpp
+++ atom_rviz_plugin/src/set_initial_estimate.cpp
@@ -4,6 +4,8 @@
 #include "std_msgs/String.h"
 #include <atom_msgs/SetSensorInteractiveMarker.h>
 
+#include <map>
+
 #include "ui_calibration_panel.h"
 
 namespace atom_rviz_plugin
@@ -13,11 +15,18 @@ namespace atom_rviz_plugin
       ROS_INFO_STREAM("Read the sensor on combobox;then, read the service to check scale of the marker and if its visible or not");
 
       std::string combobox_sensor = ui_->initEstimateSensorsComboBox->currentText().toUtf8().constData();
+      std::string service_name = "/set_initial_estimate/" + combobox_sensor + "/set_sensor_interactive_marker";
 
-      // Code to read the service for the combobox_sensor (server)
+      ros::ServiceClient client = nh.serviceClient<atom_msgs::GetSensorInteractiveMarker>(service_name);
 
-      ui_->initialEstimateCheckBox->setChecked(true); //change 'false' for the value read from service
-      ui_->initialEstimateSpinBox->setValue(1); //change '1' for the value read from service
+      atom_msgs::GetSensorInteractiveMarker srv;
+      //  Check http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28c%2B%2B%29
+
+/*      std::map<bool, double> state;
+//      std::cout << ret << std::endl;
+
+      ui_->initialEstimateCheckBox->setChecked(state(0)); //change 'false' for the value read from service
+      ui_->initialEstimateSpinBox->setValue(state(1)); //change '1' for the value read from service*/
 
     } // function initEstimateComboBoxTextChanged()
 
@@ -27,15 +36,13 @@ namespace atom_rviz_plugin
       std::string combobox_sensor = ui_->initEstimateSensorsComboBox->currentText().toUtf8().constData();
       std::string service_name = "/set_initial_estimate/" + combobox_sensor + "/set_sensor_interactive_marker";
 
-      //Service clients should not be transient, which means they should be initialized in the class construction and then used here
-      // TODO Put this is the constructor
       ros::ServiceClient client = nh.serviceClient<atom_msgs::SetSensorInteractiveMarker>(service_name);
 
       atom_msgs::SetSensorInteractiveMarker srv;
       //  Check http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28c%2B%2B%29
 
       srv.request.visible = ui_->initialEstimateCheckBox->isChecked();
-      srv.request.scale = ui_->initialEstimateSpinBox->value();;
+      srv.request.scale = ui_->initialEstimateSpinBox->value();
 
       int ret = client.call(srv);
       std::cout << ret << std::endl;
